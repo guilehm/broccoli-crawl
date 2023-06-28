7303a1d6dc744665fc196166f2ddf7b6f43ea8f0
gui <guihm.dev@gmail.com>
2023-06-27 23:14:15 -0300

    do not obey robots.txt

--- broccoli/settings.py
+++ broccoli/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = "broccoli.spiders"
 #USER_AGENT = "broccoli (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
