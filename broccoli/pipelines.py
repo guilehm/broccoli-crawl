f18a432f418c643e2ecd299014a11c496585f8d0
gui <guihm.dev@gmail.com>
2023-07-02 23:50:44 -0300

    insert items on database

--- broccoli/pipelines.py
+++ broccoli/pipelines.py
@@ -2,12 +2,18 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
+import os
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
+from pymongo import MongoClient
+
+client = MongoClient(os.getenv("MONGODB_URL"))
+db = client["brocooli"]
+
 
 class BroccoliPipeline:
     def process_item(self, item, spider):
+        db[spider.name].insert_one(dict(item))
         return item
